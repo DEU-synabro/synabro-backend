0b74e5dbdc578825796f7f41138a62668bdf83ab
jiy00nn <wldbs204@gmail.com>
2021-12-29 22:20:27 +0900

    Initial Commit

---
+++ src/main/java/com/deu/synabro/SynabroApplication.java
@@ -0,0 +1,13 @@
+package com.deu.synabro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynabroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynabroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deu/synabro/SynabroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deu.synabro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynabroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
