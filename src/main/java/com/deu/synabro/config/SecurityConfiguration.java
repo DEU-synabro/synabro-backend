424e954bb4a38ad4db7d38191ea2bc005ca707fe
jiy00nn <wldbs204@gmail.com>
2022-01-04 11:03:50 +0900

    docs: Edit description about hal browser

--- src/main/java/com/deu/synabro/config/SecurityConfiguration.java
+++ src/main/java/com/deu/synabro/config/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.deu.synabro.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
