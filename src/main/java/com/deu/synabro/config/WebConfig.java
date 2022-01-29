ce8164a54cef5fd592dbc9b428a6ddcacb002249
jiy00nn <wldbs204@gmail.com>
2022-01-30 02:55:08 +0900

    docs: Add description for javadoc

--- src/main/java/com/deu/synabro/config/WebConfig.java
+++ src/main/java/com/deu/synabro/config/WebConfig.java
@@ -10,6 +10,14 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+
+/*
+ * 이 클래스는 Swagger UI 및 Cors 정책과 관련된
+ * 초기 설정을 하기 위한 클래스이다.
+ * @author Jiyoon Bak
+ * @version 1.0
+ * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     /*
@@ -25,6 +33,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000");
     }
     */
+
+    /*
+     * Swagger 설정의 핵심이 되는 Bean으로
+     * api의 그룹명이나 이동경로, 보여질 api가 속한 패키지 등의
+     * 자세한 정보를 담습니다.
+     *
+     * @return API 문서 적용될 설정
+     * @see springfox.documentation.spring.web.plugins.Docket
+     */
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
@@ -36,6 +53,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .apiInfo(apiInfo());
     }
 
+    /*
+     * Swagger를 이용한 API 문서의
+     * 대표적인 내용(제목, 설명, 버전 등)을 표시할 수 있도록
+     * ApiInfo 객체를 생성한다.
+     *
+     * @return API 문서에 적용될 내용들
+     * @see springfox.documentation.service.ApiInfo
+     */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Synabro")
