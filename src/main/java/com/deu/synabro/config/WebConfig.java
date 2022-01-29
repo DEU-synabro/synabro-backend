a4bc46c15a55a6744281f99730e95eb590e1879e
jiy00nn <wldbs204@gmail.com>
2022-01-30 02:43:27 +0900

    feature: Add swagger UI

--- src/main/java/com/deu/synabro/config/WebConfig.java
+++ src/main/java/com/deu/synabro/config/WebConfig.java
@@ -1,9 +1,14 @@
 package com.deu.synabro.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,9 +18,29 @@ public class WebConfig implements WebMvcConfigurer {
      *
      * ※ 크로스 도메인 이슈 : 브라우저에서 다른 도메인으로 URL 요청을 하는 경우 나타나는 보안 문제
      */
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000");
-//    }
+    /*
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+    */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                    .apis(RequestHandlerSelectors.basePackage("com.deu.synabro"))
+                    .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Synabro")
+                .description("Synabro API docs")
+                .version("1.0")
+                .build();
+    }
 }
