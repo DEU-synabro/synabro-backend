bbb684bd7638b40839c15e63f598d073379a3452
tkfdkskarl56 <tkfdkskarl56@naver.com>
2022-02-07 10:52:07 +0900

    board-refactoring-2

--- src/main/java/com/deu/synabro/entity/Board.java
+++ src/main/java/com/deu/synabro/entity/Board.java
@@ -18,20 +18,21 @@ public class Board extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="user_id")
+    @Column(name="userid")
     private String userid;
-    @Column
+    @Column(name="title")
     private String title;
-    @Column
+    @Column(name="contents")
     private String contents;
-    @Column
-    private BoardType boardType;
+    @Enumerated(value=EnumType.STRING)
+    @Column(name="boardtype")
+    private BoardType boardtype;
 
     @Builder
-    public Board(String userid, String title, String contents, BoardType boardType) {
+    public Board(String userid, String title, String contents, BoardType boardtype) {
         this.userid = userid;
         this.title = title;
         this.contents = contents;
-        this.boardType = boardType;
+        this.boardtype = boardtype;
     }
 }
--- src/main/java/com/deu/synabro/repository/BoardRepository.java
+++ src/main/java/com/deu/synabro/repository/BoardRepository.java
@@ -2,19 +2,21 @@ package com.deu.synabro.repository;
 
 import com.deu.synabro.entity.enums.BoardType;
 import com.deu.synabro.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RepositoryRestResource
+@Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
-    List<Board> findByTitleContaining(String title);
-    List<Board> findByUserid(String userid);
+    Page<Board> findByTitleContaining(Pageable pageable, String title);
+    Page<Board> findByUserid(Pageable pageable, String userid);
+    Page<Board> findByTitleContainingOrContentsContaining(Pageable pageable, String title, String content);
     List<Board> deleteByTitle(String title);
-    List<Board> findByTitleContainingOrContentsContaining(String title, String content);
-    List<Board> findByUseridAndTitle(String userid, String title);
     List<Board> findByIdAndTitle(Long id, String title);
-    List<Board> findByBoardType(@Param("boardType") BoardType boardType);
+//    List<Board> findByBoardType(@Param("boardType") BoardType boardType);
 }
