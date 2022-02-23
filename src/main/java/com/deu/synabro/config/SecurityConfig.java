2a22f3420a234c1efef32c47be5c6059d1aae040
jiy00nn <wldbs204@gmail.com>
2022-02-24 07:17:42 +0900

    chore: delete permit url

--- src/main/java/com/deu/synabro/config/SecurityConfig.java
+++ src/main/java/com/deu/synabro/config/SecurityConfig.java
@@ -51,9 +51,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeHttpRequests()
                 .antMatchers( "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs").permitAll()
-                .antMatchers("/api/auth").permitAll()
                 .antMatchers("/api/boards/**").permitAll()
-                .antMatchers("/api/members/**").permitAll()
+                .antMatchers("/api/members/signin").permitAll()
+                .antMatchers("/api/members/signup").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
