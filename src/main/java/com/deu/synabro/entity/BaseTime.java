fd29edbe937d7d00af4688740fc0ff6a49441a6f
tkfdkskarl56 <tkfdkskarl56@naver.com>
2022-04-07 14:57:16 +0900

    fix-board DateFormat

--- src/main/java/com/deu/synabro/entity/BaseTime.java
+++ src/main/java/com/deu/synabro/entity/BaseTime.java
@@ -20,12 +20,12 @@ public abstract class  BaseTime {
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @CreatedDate
-    @Schema(description = "사용자 계정 생성 시간", example = "2021-11-26T06:32:14.168")
+    @Schema(description = "사용자 계정 생성 시간", example = "2021-11-26 06:32:14")
     private LocalDateTime createdDate;
 
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @LastModifiedDate
-    @Schema(description = "사용자 계정 정보 마지막 수정 시간", example = "2022-01-02T12:23:48.753")
+    @Schema(description = "사용자 계정 정보 마지막 수정 시간", example = "2022-01-02 12:23:48")
     private LocalDateTime updatedDate;
 }
