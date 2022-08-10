798bbf4fe7613abfab6ef1eb9434a4d27a361f2d
tkfdkskarl56 <tkfdkskarl56@naver.com>
2022-08-10 22:58:26 +0900

    feat: Add Applicant List View Method

--- src/main/java/com/deu/synabro/repository/OffVolunteerApplicationRepository.java
+++ src/main/java/com/deu/synabro/repository/OffVolunteerApplicationRepository.java
@@ -4,10 +4,12 @@ import com.deu.synabro.entity.OffVolunteerApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OffVolunteerApplicationRepository extends JpaRepository<OffVolunteerApplication, UUID> {
     Optional<OffVolunteerApplication> findByNameAndPassword(String name, String password);
+    List<OffVolunteerApplication> findByOffVolunteerId(UUID uuid);
 }
